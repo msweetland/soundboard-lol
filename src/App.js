cd218dc45ead98ffa7d83bd1cee1831dbd4d7663
Michael Sweetland <mjsweet@umich.edu>
2017-12-24 23:08:07 -0800

    async audio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      moneySound: 'http://soundbible.com/mp3/Cash%20Register%20Cha%20Ching-SoundBible.com-184076484.mp3',
+      rapHornSound: 'https://www.myinstants.com/media/sounds/air-horn-club-sample_1.mp3'
+    };
+  }
+
+  playSound = (sound) => {
+    new window.Audio(sound).play();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="stage">
+        <button className="btn btn-info" onClick={() => this.playSound(this.state.moneySound)}>cash money</button>
+        <button className="btn btn-info" onClick={() => this.playSound(this.state.rapHornSound)}>rapHorn</button>
       </div>
     );
   }
